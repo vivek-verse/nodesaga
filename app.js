3b4f6075d37990003ec3c47350eb96087ff69e0b
Vivek Yadav <vivek@beusalons.com>
2019-01-19 22:31:40 +0530

    Legends pound back.

--- app.js
+++ app.js
@@ -26,50 +26,102 @@ var fallback2 = function(){
 
 module.exports = {
 	
-	StartTransaction   : function(taskQueue){
+	StartTransaction   : function(taskQueue, startCallback){
 		console.log("StartTransaction called.");
 		var queueArray = taskQueue;
-		this.SetTransactionQueue(taskQueue);
+		this.SetTransactionQueue(taskQueue, startCallback);
 	},
-	LoopQueue           : function(taskQueue, queueStates, currentIndex, callback){
-
+	ExecuteTask         : function(taskQueue, currentIndex, executeCallback){
 	    var taskPromise = promisify(taskQueue[currentIndex].task);
 
 	    taskPromise().then(function(str){
-	    	console.log(str)
+	    	executeCallback(null, str);
 	    }).catch(function(err){
-	    	console.log("RunFallbacks");
+	    	executeCallback(new Error("Failed at index ", currentIndex), null);
 	    })
+	},
+	CheckQueueStack     : function(queueStates, checkQueueCallback){
+		var failed = false;
 
-	    if(currentIndex < taskQueue.length - 1){
-	    	currentIndex++;
-	    	this.LoopQueue(taskQueue, queueStates, currentIndex, callback);
+		for(var key in queueStates){
+				if(queueStates[key].ran != "yes" || queueStates[key].executed != "yes"){
+					failed = true;
+				}
+		}
+
+		if(failed){
+			checkQueueCallback(new Error("Failed to execute all transaxtion"), null);
+		}else{
+			checkQueueCallback(null, "All executed successfully");
+		}
+
+	},
+	LoopQueue           : function(taskQueue, queueStates, currentIndex, startCallback){
+
+		var that = this;
+
+	    if(currentIndex < taskQueue.length){
+	    	that.ExecuteTask(taskQueue, currentIndex, function(err, done){
+	    		if(done){
+
+	    			for(var key in queueStates){
+	    				if(key == "task"+currentIndex){
+	    					queueStates[key].ran = "yes";
+	    					queueStates[key].executed = "yes";
+	    				}
+	    			}
+
+					currentIndex++;
+			    	that.LoopQueue(taskQueue, queueStates, currentIndex, startCallback);
+	    		}else{
+
+	    			for(var key in queueStates){
+	    				if(key == "task"+currentIndex){
+	    					queueStates[key].ran = "yes";
+	    					queueStates[key].executed = "failed";
+	    				}
+	    			}
+
+	    			that.CheckQueueStack(taskQueue, function(err, done){
+			   			if(err){
+
+			   			}else{
+					    	startCallback(null, done);
+			   			}
+			   		});
+
+	    		}
+	    	})
 	    }else{
-	    	callback(null, "All forward");
+	   		that.CheckQueueStack(queueStates, function(err, done){
+	   			if(err){
+
+	   			}else{
+			    	startCallback(null, done);
+	   			}
+	   		});
 	    }
 
 	},
-	ExecuteTransaction  : function(queueStates, taskQueue){
-		console.log("ExecuteTransaction called.");
+	ExecuteTransaction  : function(queueStates, taskQueue, startCallback){
 		var queueLength      = taskQueue.length,
 	    	currentIndex 	 = 0;
-	    	this.LoopQueue(taskQueue, queueStates, currentIndex, function(err, done){
+	    	this.LoopQueue(taskQueue, queueStates, currentIndex, startCallback, function(err, done){
 	    		console.log(done);
 	    	})
-
 	},
-	SetTransactionQueue : function(taskQueue){
+	SetTransactionQueue : function(taskQueue, startCallback){
 		
 		var queueStates = new Object();
 
 		for(var i = 0; i < taskQueue.length; i++){
 			queueStates["task"+i] = {
 				"ran"      : "no", //will check if the task is executed or not
-				"executed" : "no"  // // there will be two other states yes and no
+				"executed" : "no"  //there will be two other states yes and failed
 			}
 		}
 
-		this.ExecuteTransaction(queueStates, taskQueue);
+		this.ExecuteTransaction(queueStates, taskQueue, startCallback);
 
 	},
 	RunFallbacks        : function(fallbacks){
